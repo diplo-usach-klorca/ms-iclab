08e6b18114c42ae79e99ed0594c60a80ac23a022
Marco Michea <marco.michea.sb@gmail.com>
2022-12-15 21:17:17 -0300

    Add test for ValidaApiCovid and EstadoPais

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.devopsusach2020;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devopsusach2020.model.Pais;
+import com.devopsusach2020.model.RespuestaAPICovid;
+import com.devopsusach2020.rest.RestData;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
+	RestData controller = new RestData();
+
+	@Test 
+	public void testValidaApiCovid() throws Exception {
+		int expectedStatus = 200;
+		RespuestaAPICovid response = controller.validaApiCovid();
+		assertEquals(expectedStatus, response.getStatusCode());
+	}
+
 	@Test
-	void contextLoads() {
+	public void testEstadoPais() throws Exception {	
+
+		String pais = "Ecuador";
+		Pais response = controller.getTotalPais(pais);
+		assertEquals( pais.toLowerCase(), response.getCountry().toLowerCase());
+
 	}
 
 }
