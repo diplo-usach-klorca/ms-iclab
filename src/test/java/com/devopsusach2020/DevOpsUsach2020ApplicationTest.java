c85e6fdbfac3a8ea72bb22ff7d959dbfbcb03700
Kevin Lorca <klorca@inovabiz.com>
2022-12-18 14:38:43 -0300

    Se configuran test unitarios para que se ejecuten al compilar

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTest.java
@@ -0,0 +1,31 @@
+package com.devopsusach2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.devopsusach2020.model.RespuestaAPICovid;
+import com.devopsusach2020.model.Pais;
+import com.devopsusach2020.rest.RestData;
+
+@SpringBootTest
+public class DevOpsUsach2020ApplicationTest {
+	RestData controller = new RestData();
+
+	@Test
+	public void testValidaApiCovid() throws Exception {
+		int expectedStatus = 200;
+		RespuestaAPICovid response = controller.validaApiCovid();
+
+		assertEquals(expectedStatus, response.getStatusCode());
+	}
+
+	@Test
+	public void testEstadoPais() throws Exception {
+
+		String pais = "Ecuador";
+		Pais response = controller.getTotalPais(pais);
+		assertEquals(pais.toLowerCase(), response.getCountry().toLowerCase());
+
+	}
+
+}
